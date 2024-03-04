cedf4620a560b48e0cb92a285387fa2385c2075f
MathieuMdg <151543011+MathieuMdg@users.noreply.github.com>
2024-03-04 09:17:18 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame
+from game import Game
+
+pygame.init()
+
+pygame.display.set_caption("Fruit Ninja")
+screen = pygame.display.set_mode((1000, 600))
+
+background = pygame.image.load('assets/fruit ninja.jpg')
+
+running = True
+
+# Charger le jeu
+game = Game()
+
+# Boucle tant que la condition est vraie
+while running:
+
+    # Appliquer l'arrière plan de notre jeu
+    screen.blit(background, (0, 0)) # Pour repositionner le fond d'écran changer les nombres
+
+    # Appliquer l'image du fruit
+    screen.blit(game.pasteque.image, game.pasteque.rect)
+
+    # Mettre à jour l'écran
+    pygame.display.flip()
+
+    # Si le joueur ferme la fenêtre (ou clique sur le bouton cliquer [plus tard])
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            print("Fermeture du jeu")
+
+        #Detecter si un joueur lâche une touche du clavier
+        elif event.type == pygame.KEYDOWN:
+            # quelle touche a été utilisé
+            if event.key == pygame.K_LEFT: #Marche pas encore mais désigne le clique de la souris
+                print("Découpe enclenchée")
+                #print(event.button)  # numéro du bouton
+                if event.type == pygame.MOUSEMOTION:
+                    print(event.pos) #Coordonnées du clique
+                    pygame.draw.circle(pygame.display.set.mode((800,600)), (0,0,0), event.pos, 40) #Trace un rond à la position de la souris
